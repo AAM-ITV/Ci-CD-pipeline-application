070e32e64442bf82f0e95c1457addee8077e944e
AAM-ITV <maioroff.andrey@gmail.com>
2025-01-18 21:58:06 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -52,11 +52,11 @@ resource "yandex_vpc_subnet" "subnet-1" {
   v4_cidr_blocks = ["192.168.10.0/24"]
 }
 
-output "internal_ip_address_vm_1" {
+output "internal_ip_address_prod-stand" {
   value = yandex_compute_instance.vm-1.network_interface.0.ip_address
 }
 
 
-output "external_ip_address_vm_1" {
+output "external_ip_address_prod-stand" {
   value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
 }
